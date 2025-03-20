b5cb4914fe2a6aab7c3d02281c5d875b0de4d0d9
Aleksandr Fedorov <sasha.fedad@mail.ru>
2025-03-20 13:00:41 +0300

    исправлено дублирование данных в FormModel

--- src/types/index.ts
+++ src/types/index.ts
@@ -141,102 +141,108 @@ export interface IFormModel {
     getOrderData(): object;
 }
 
-export class FormModel implements IFormModel {
-    paymentMethod: string;
-    email: string;
-    phone: string;
-    address: string;
-    total: number;
-    items: string[];
-    formErrors: IOrderFormErrors = {};
-
-    constructor(protected events: IEvents) {
-        this.paymentMethod = '';
-        this.email = '';
-        this.phone = '';
-        this.address = '';
-        this.total = 0;
-        this.items = [];
-    }
-
-    setAddress(field: string, value: string) {
-        if (field === 'address') {
-            this.address = value;
-        }
-
-        if (this.validateAddress()) {
-            this.events.emit('order:ready', this.getOrderData());
-        }
-    }
-
-    validateAddress() {
-        const addressRegex = /^[а-яА-ЯёЁa-zA-Z0-9\s\/.,-]{7,}$/;
-        const errors: typeof this.formErrors = {};
-
-        if (!this.address) {
-            errors.address = 'Необходимо указать адрес';
-        } else if (!addressRegex.test(this.address)) {
-            errors.address = 'Укажите настоящий адрес';
-        } else if (!this.paymentMethod) {
-            errors.payment = 'Выберите способ оплаты';
-        }
-
-
-        this.formErrors = errors;
-        this.events.emit('formErrors:address', this.formErrors);
-        return Object.keys(errors).length === 0;
-    }
-
-    setContactInfo(field: string, value: string) {
-        if (field === 'email') {
-            this.email = value;
-        } else if (field === 'phone') {
-            this.phone = value;
-        }
-
-        if (this.validateContactInfo()) {
-            this.events.emit('order:ready', this.getOrderData());
-        }
-    }
-
-    validateContactInfo() {
-        const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-        const phoneRegex = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{10}$/;
-        const errors: typeof this.formErrors = {};
-
-        if (!this.email) {
-            errors.email = 'Необходимо указать email';
-        } else if (!emailRegex.test(this.email)) {
-            errors.email = 'Некорректный адрес электронной почты';
-        }
-
-        if (this.phone.startsWith('8')) {
-            this.phone = '+7' + this.phone.slice(1);
-        }
-
-        if (!this.phone) {
-            errors.phone = 'Необходимо указать телефон';
-        } else if (!phoneRegex.test(this.phone)) {
-            errors.phone = 'Некорректный формат номера телефона';
-        }
-
-        this.formErrors = errors;
-        this.events.emit('formErrors:change', this.formErrors);
-        return Object.keys(errors).length === 0;
-    }
-
-    getOrderData() {
-        return {
-            payment: this.paymentMethod,
-            email: this.email,
-            phone: this.phone,
-            address: this.address,
-            total: this.total,
-            items: this.items,
-        }
-    }
+export interface IFormModel {
+  paymentMethod: string;
+  email: string;
+  phone: string;
+  address: string;
+  setAddress(field: string, value: string): void;
+  validateAddress(): boolean;
+  setContactInfo(field: string, value: string): void;
+  validateContactInfo(): boolean;
+  getOrderData(items: string[], total: number): IOrderBatch;
 }
 
+export class FormModel implements IFormModel {
+  paymentMethod: string;
+  email: string;
+  phone: string;
+  address: string;
+  formErrors: IOrderFormErrors = {};
+
+  constructor(protected events: IEvents) {
+      this.paymentMethod = '';
+      this.email = '';
+      this.phone = '';
+      this.address = '';
+  }
+
+  setAddress(field: string, value: string) {
+      if (field === 'address') {
+          this.address = value;
+      }
+
+      if (this.validateAddress()) {
+          this.events.emit('order:ready', this.getOrderData([], 0)); // Здесь передаются пустые значения, так как items и total будут переданы позже
+      }
+  }
+
+  validateAddress() {
+      const addressRegex = /^[а-яА-ЯёЁa-zA-Z0-9\s\/.,-]{7,}$/;
+      const errors: typeof this.formErrors = {};
+
+      if (!this.address) {
+          errors.address = 'Необходимо указать адрес';
+      } else if (!addressRegex.test(this.address)) {
+          errors.address = 'Укажите настоящий адрес';
+      } else if (!this.paymentMethod) {
+          errors.payment = 'Выберите способ оплаты';
+      }
+
+      this.formErrors = errors;
+      this.events.emit('formErrors:address', this.formErrors);
+      return Object.keys(errors).length === 0;
+  }
+
+  setContactInfo(field: string, value: string) {
+      if (field === 'email') {
+          this.email = value;
+      } else if (field === 'phone') {
+          this.phone = value;
+      }
+
+      if (this.validateContactInfo()) {
+          this.events.emit('order:ready', this.getOrderData([], 0)); // Здесь передаются пустые значения, так как items и total будут переданы позже
+      }
+  }
+
+  validateContactInfo() {
+      const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+      const phoneRegex = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{10}$/;
+      const errors: typeof this.formErrors = {};
+
+      if (!this.email) {
+          errors.email = 'Необходимо указать email';
+      } else if (!emailRegex.test(this.email)) {
+          errors.email = 'Некорректный адрес электронной почты';
+      }
+
+      if (this.phone.startsWith('8')) {
+          this.phone = '+7' + this.phone.slice(1);
+      }
+
+      if (!this.phone) {
+          errors.phone = 'Необходимо указать телефон';
+      } else if (!phoneRegex.test(this.phone)) {
+          errors.phone = 'Некорректный формат номера телефона';
+      }
+
+      this.formErrors = errors;
+      this.events.emit('formErrors:change', this.formErrors);
+      return Object.keys(errors).length === 0;
+  }
+
+  getOrderData(items: string[], total: number): IOrderBatch {
+      return {
+          payment: this.paymentMethod,
+          email: this.email,
+          phone: this.phone,
+          address: this.address,
+          total: total,
+          items: items,
+      };
+  }
+}
 
 
 export interface IBasket {
